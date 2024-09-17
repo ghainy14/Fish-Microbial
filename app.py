472b962c28c318fe8b3a58e76ba9fd49aa77f10a
Afolabi-Yusuf Ganiyat Kemi <91464796+ghainy14@users.noreply.github.com>
2024-09-17 14:16:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler, OneHotEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.multioutput import MultiOutputClassifier
-from sklearn.metrics import classification_report
+
 import joblib
 model = joblib.load('microbial_model.pkl')
 
